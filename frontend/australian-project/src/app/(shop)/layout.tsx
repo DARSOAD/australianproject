94b2bbe86d4a4fab71db202e8d6d884e481127c3
Sebas <sebastianbuenoramirez@gmail.com>
2024-12-27 19:32:33 -0500

    Feature: creacion del grid y de las carts

--- frontend/australian-project/src/app/(shop)/layout.tsx
+++ frontend/australian-project/src/app/(shop)/layout.tsx
@@ -1,3 +1,4 @@
+import { TopMenu } from "@/components";
 
 export default function shopLayout({
  children
@@ -5,8 +6,11 @@ export default function shopLayout({
  children: React.ReactNode;
 }) {
   return (
-    <main className="min-h-screen bg-red-500">
-      {children}
+    <main className="min-h-screen">
+      <TopMenu/>
+      <div className='px-0 sm:px-10'>
+        {children}
+      </div>
     </main>
   );
 }
