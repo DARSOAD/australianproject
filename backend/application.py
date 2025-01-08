61bf047a365fc874a71fab48b15d4c40cdc2b1b1
Diego Rodríguez <dieguitotoi@gmail.com>
2025-01-07 19:37:30 -0500

    Realizando ajustes en varios archivos para qure corra bien en Elastic Beanstalk

--- backend/application.py
+++ backend/application.py
@@ -22,4 +22,4 @@ app.mount("/users", users_app)  # Rutas de usuarios
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
--- backend/users/routers/users.py
+++ backend/users/routers/users.py
@@ -6,6 +6,10 @@ router = APIRouter(prefix="/users",
                 tags=["users"],
                 responses={status.HTTP_404_NOT_FOUND:{"message":"No encontrado"}})
 
+@router.get("/")
+async def get_users():
+    return {"message": "Users endpoint working"}
+
 @router.post("/register")
 async def register_user(user: User):
     return {"message": "User registered successfully XD"}
