bf2883d6b921e7a3a567b8271755f8591f6dffb3
Jenny <alexajennyvalero@gmail.com>
2025-04-08 00:09:57 -0500

    6: Búsqueda específica de productos

--- frontend/australian-project/src/app/api/catalog/route.tsx
+++ frontend/australian-project/src/app/api/catalog/route.tsx
@@ -2,10 +2,10 @@ import { NextResponse } from 'next/server';
 
 // Lista de productos falsa (de prueba)
 const mockProducts = [
-    { id: 1, name: 'Zapatos Rosados', price: 36.0, priceWithMembership: 23.1 },
-    { id: 2, name: 'Cartera Fucsia' },
-  { id: 3, name: 'Vestido Rosa Pastel' },
-  { id: 4, name: 'Zapatos Negros' },
+    { id: 1, name: 'Zapatos Rosados 1', price: 36.0, priceWithMembership: 23.1 },
+    { id: 2, name: 'Cartera Fucsia 1' },
+  { id: 3, name: 'Vestido Rosa Pastel 1' },
+  { id: 4, name: 'Zapatos Negros 1 ' },
 ];
 
 export async function GET(req: Request) {
